1a0d15fde4ebf7e17bf70771eb9f637177c2863e
BrVenturaU <brandonventura16@gmail.com>
2024-01-12 09:28:32 -0600

    Add stetic styles.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "slide-down": "slide-down var(--a-duration) var(--a-delay) ease-in 1 both",
+      },
+      keyframes: {
+        "slide-down": {
+          from: { transform: "translateY(-200%)" },
+          to: { transform: "translateY(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
